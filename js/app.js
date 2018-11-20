494d4d8c237d6be3fe10e87f1eece196dce79282
panmm <2739482158@qq.com>
2018-11-20 16:41:09 +0800

    打包设置

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ window.app = {
 	/**
 	 * netty服务后端发布的url地址
 	 */
-	nettyServerUrl: 'ws://192.168.65.228:8088/ws',
+//	nettyServerUrl: 'ws://192.168.65.228:8088/ws',
+	nettyServerUrl: 'ws://192.168.184.1:8088/ws',
 	
 	/**
 	 * 后端服务发布的url地址
 	 */
-	serverUrl: 'http://192.168.65.228:8080',
+//	serverUrl: 'http://192.168.65.228:8080',
+	serverUrl: 'http://192.168.184.1:8080',
 	
 	/**
 	 * 图片服务器的url地址
